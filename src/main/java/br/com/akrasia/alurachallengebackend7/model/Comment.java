b635917722cd1f5fb0b3ba6435266643cfcc6156
Jhonatan Yuji <jhonatan-yuji@hotmail.com>
2023-08-08 02:54:39 -0300

    Define Comment attributes

--- src/main/java/br/com/akrasia/alurachallengebackend7/model/Comment.java
+++ src/main/java/br/com/akrasia/alurachallengebackend7/model/Comment.java
@@ -1,5 +1,18 @@
 package br.com.akrasia.alurachallengebackend7.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Comment {
+
+    @Id
+    private Long id;
+
+    private String author;
+
+    private String content;
+
+    private String filePath;
 
 }
