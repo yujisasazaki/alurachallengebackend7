fbd824fdcfcecca9ef91500dc7df6db30b1bf6a7
Jhonatan Yuji <jhonatan-yuji@hotmail.com>
2023-08-08 15:34:03 -0300

    Use text instead of varchar

--- src/main/resources/db/migration/V1__create_photo_table.sql
+++ src/main/resources/db/migration/V1__create_photo_table.sql
@@ -1,5 +1,8 @@
 CREATE TABLE photo (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name VARCHAR(255),
-  type VARCHAR(255)
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name TEXT NOT NULL,
+	type TEXT NOT NULL,
+
+	CONSTRAINT ck_photo_name CHECK (char_length(name) <= 255),
+	CONSTRAINT ck_photo_type CHECK (char_length(type) <= 255)
 );
