f1f3f6f130ae56be546ec7d9c45dd5f1c3fb9229
Jhonatan Yuji <jhonatan-yuji@hotmail.com>
2023-08-11 13:25:11 -0300

    Create global exception handler

---
+++ src/main/java/br/com/akrasia/alurachallengebackend7/controller/ExceptionHandlerController.java
@@ -0,0 +1,24 @@
+package br.com.akrasia.alurachallengebackend7.controller;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<String> handleIOException(IOException e) {
+        return ResponseEntity.badRequest().body("Error saving photo");
+    }
+    
+}
