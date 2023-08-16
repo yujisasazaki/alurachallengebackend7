062cbd8f75b555faf25e70efb15c854899a46604
Jhonatan Yuji <jhonatan-yuji@hotmail.com>
2023-08-16 15:15:36 -0300

    Added Cors

--- src/main/java/br/com/akrasia/alurachallengebackend7/Alurachallengebackend7Application.java
+++ src/main/java/br/com/akrasia/alurachallengebackend7/Alurachallengebackend7Application.java
@@ -2,12 +2,28 @@ package br.com.akrasia.alurachallengebackend7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Alurachallengebackend7Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Alurachallengebackend7Application.class, args);
+	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				System.out.println("corsConfigurer");
+				registry.addMapping("/**")
+					.allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 }
