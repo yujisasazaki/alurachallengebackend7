3e76ad2f0fa606132977dce3d78c1200a492b2fc
Jhonatan Yuji <jhonatan-yuji@hotmail.com>
2023-09-01 13:54:55 -0300

    Return json instead of string in ExceptionHandler

--- src/main/java/br/com/akrasia/alurachallengebackend7/controller/ExceptionHandlerController.java
+++ src/main/java/br/com/akrasia/alurachallengebackend7/controller/ExceptionHandlerController.java
@@ -3,6 +3,7 @@ package br.com.akrasia.alurachallengebackend7.controller;
 import java.io.IOException;
 import java.util.NoSuchElementException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,27 +16,42 @@ import jakarta.persistence.EntityNotFoundException;
 public class ExceptionHandlerController {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
-        return ResponseEntity.status(404).body(e.getMessage());
+    public ResponseEntity<ErrorMessage> handleEntityNotFoundException(EntityNotFoundException e) {
+        String message = e.getMessage();
+        return new ResponseEntity<>(new ErrorMessage(message), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
-        return ResponseEntity.status(404).body(e.getMessage());
+    public ResponseEntity<ErrorMessage> handleNoSuchElementException(NoSuchElementException e) {
+        String message = e.getMessage();
+        return new ResponseEntity<>(new ErrorMessage(message), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<String> handleIOException(IOException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ErrorMessage> handleIOException(IOException e) {
+        String message = e.getMessage();
+        return new ResponseEntity<>(new ErrorMessage(message), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class) 
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         BindingResult result = e.getBindingResult();
-        String errorMessage = result.getFieldErrors().stream()
+        String message = result.getFieldErrors().stream()
             .map(fieldError -> "Field " + fieldError.getField() + " " + fieldError.getDefaultMessage())
             .reduce("", (acc, fieldError) -> acc + fieldError + "\n");
         
-        return ResponseEntity.badRequest().body(errorMessage);
-    }    
+        return new ResponseEntity<>(new ErrorMessage(message), HttpStatus.BAD_REQUEST);
+    }
+
+    private static class ErrorMessage {
+        private final String message;
+
+        public ErrorMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 }
