8f475d6d228992117dc802b399d35a3c24a790be
Jhonatan Yuji <jhonatan-yuji@hotmail.com>
2023-09-01 13:30:34 -0300

    Handle MethodArgumentNotValidException

--- src/main/java/br/com/akrasia/alurachallengebackend7/controller/ExceptionHandlerController.java
+++ src/main/java/br/com/akrasia/alurachallengebackend7/controller/ExceptionHandlerController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,5 +28,14 @@ public class ExceptionHandlerController {
     public ResponseEntity<String> handleIOException(IOException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
-    
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) 
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+        String errorMessage = result.getFieldErrors().stream()
+            .map(fieldError -> "Field " + fieldError.getField() + " " + fieldError.getDefaultMessage())
+            .reduce("", (acc, fieldError) -> acc + fieldError + "\n");
+        
+        return ResponseEntity.badRequest().body(errorMessage);
+    }    
 }
